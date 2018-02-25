111da98e9c8e1a54f98fe7781d4183575eeaab7a
mike <sorokin.michael.96@yandex.ru>
2018-02-25 16:51:09 +0300

    compression matrix and SBox for picaro cipher

--- picaro/round_function.c
+++ picaro/round_function.c
@@ -2,3 +2,8 @@
 // Created by hp on 25.02.18.
 //
 
+void key_add(unsigned char * text, unsigned char * key, unsigned size) {
+    for (int i = 0; i < size; ++i) {
+        text[i] ^= key[i];
+    }
+}
